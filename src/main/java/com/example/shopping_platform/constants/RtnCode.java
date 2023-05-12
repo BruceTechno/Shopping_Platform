1c4cfda7fea310fcf709d727fc0de5e80369ba18
Bruce <yu86923@gmail.com>
2023-05-12 12:25:54 +0800

    first commit

---
+++ src/main/java/com/example/shopping_platform/constants/RtnCode.java
@@ -0,0 +1,4 @@
+package com.example.shopping_platform.constants;
+
+public enum RtnCode {
+}
---
+++ src/main/resources/schema-mysql.sql
@@ -0,0 +1,54 @@
+CREATE TABLE IF NOT EXISTS `people` (
+  `account` VARCHAR(20) NOT NULL,
+  `pwd` VARCHAR(20) NULL,
+  `name` VARCHAR(45) NULL,
+  `address` VARCHAR(45) NULL,
+  `phone` VARCHAR(45) NULL,
+  `active` TINYINT NULL DEFAULT 0,
+  `identity` INT NULL DEFAULT 0,
+  `order_number` INT NULL DEFAULT 0,
+  PRIMARY KEY (`account`));
+
+  CREATE TABLE IF NOT EXISTS `commodity` (
+  `number` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `category` VARCHAR(45) NULL,
+  `inventory` INT NULL DEFAULT 0,
+  `price` INT NULL DEFAULT 0,
+  `status` INT NULL DEFAULT 0,
+  PRIMARY KEY (`number`));
+
+CREATE TABLE IF NOT EXISTS `order` (
+  `order_number` INT NOT NULL,
+  `account` VARCHAR(45) NULL,
+  `commodity_number` INT NULL DEFAULT 0,
+  `quantity` INT NULL DEFAULT 0,
+  `pay_way` INT NULL DEFAULT 0,
+  `delivery_way` INT NULL DEFAULT 0,
+  PRIMARY KEY (`order_number`));
+
+
+CREATE TABLE IF NOT EXISTS `shopping_car` (
+  `serial_number` INT NOT NULL AUTO_INCREMENT,
+  `account` VARCHAR(45) NULL,
+  `commodity_number` INT NULL DEFAULT 0,
+  `quantity` INT NULL DEFAULT 0,
+  PRIMARY KEY (`serial_number`));
+
+
+CREATE TABLE IF NOT EXISTS `identity_code` (
+  `code` INT NOT NULL,
+  `identity` VARCHAR(45) NULL,
+  PRIMARY KEY (`code`));
+
+CREATE TABLE IF NOT EXISTS `pay_way_code` (
+  `code` INT NOT NULL,
+  `pay_way` VARCHAR(45) NULL,
+  PRIMARY KEY (`code`));
+
+CREATE TABLE IF NOT EXISTS `delivery_way_code` (
+  `code` INT NOT NULL,
+  `delivery_way` VARCHAR(45) NULL,
+  PRIMARY KEY (`code`));
+
+
